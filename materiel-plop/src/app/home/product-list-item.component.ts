f1d9bf7b86a122599ff196bd504358fe76c9dc07
David MAURIN <d.maurin@groupeonepoint.com>
2018-01-17 18:41:04 +0100

    feat(component): add product binding exercise solution

--- materiel-plop/src/app/home/product-list-item.component.ts
+++ materiel-plop/src/app/home/product-list-item.component.ts
@@ -1,8 +1,10 @@
-import {Component} from '@angular/core';
+import {Component, Input} from '@angular/core';
+import {Product} from './product.model';
 
 @Component({
   selector: 'mp-product-list-item',
   templateUrl: 'product-list-item.component.html'
 })
 export class ProductListItemComponent {
+  @Input() product: Product;
 }
--- materiel-plop/src/app/home/product-list.component.ts
+++ materiel-plop/src/app/home/product-list.component.ts
@@ -1,10 +1,14 @@
 import {Component} from '@angular/core';
+import {Product} from './product.model';
 
 @Component({
   selector: 'mp-product-list',
   templateUrl: 'product-list.component.html'
 })
 export class ProductListComponent {
-  constructor() {
-  }
+  products: Array<Product> = [
+    {title: 'Surface Book', price: 1401, image: 'surface-book.jpg', review: 15},
+    {title: 'Macbook pro', price: 1699, image: 'macbook-pro.jpg', review: 10},
+    {title: 'Lenovo Yoga book', price: 579, image: 'lenovo-yoga-book.jpg', review: 3}
+  ];
 }
